e85d9e4056696dc3ed10882a582445d1da07e851
ImogenG-R <56586145+ImogenG-R@users.noreply.github.com>
2019-10-17 14:55:22 +0100

    add new

---
+++ Week-2/numberguess.py
@@ -0,0 +1,9 @@
+print('Guess a number between 1 and 10,')
+number = input()
+import random
+dice_roll = random.randint(1,10)
+if (number == dice_roll):
+    print('you were correct!')
+else:
+    print('you were wrong!')
+print(dice_roll)
